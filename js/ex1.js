5a6792e062bec8a5fbab4a61590225947f898848
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-21 00:21:01 -0300

    Finalization of exercises 1 of Eloquent JS

--- js/ex1.js
+++ js/ex1.js
@@ -4,3 +4,33 @@ export function sevenPrints( character ) {
     }
 }
 
+export function fizzBuzz() {
+    for ( let i = 1; i <= 100; i++ ){
+        if ( i % 3 == 0 && i % 5 == 0)
+            console.log ( "FizzBuzz" );
+        else if ( i % 5 == 0 )
+            console.log ( "Buzz" );
+        else if ( i % 3 == 0 )
+            console.log ( "Fizz" );
+        else
+            console.log ( i );
+    }
+}
+
+export function buildBoard( size ) {
+    const black = "#";
+    const white = " ";
+    let board = "";
+
+    for (let line = 0; line < size; line++) {
+        for (let column = 0; column < size; column++) {
+            if ( line % 2 == 0 )
+                board += column % 2 == 0 ? black : white;
+            else 
+                board += column % 2 == 0 ? white : black;
+        }
+        board += "\n"
+    }
+
+    console.log(board);
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import * as ex1 from './ex1.js'
 
 ex1.sevenPrints("#");
+ex1.fizzBuzz();
+ex1.buildBoard(4);
