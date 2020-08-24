534c70901e916f614107dcb213b4a76ce9c26b7a
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-23 23:43:04 -0300

    Exercise 2, question 1 from eloquent-js

---
+++ js/ex2.js
@@ -0,0 +1,5 @@
+export function min(n, m) {
+  if (n < m) return n;
+  return m;
+}
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-import * as ex1 from './ex1.js'
+import * as ex1 from './ex1.js';
+import * as ex2 from './ex2.js';
 
-ex1.sevenPrints("#");
+//ex1.sevenPrints("#");
 ex1.fizzBuzz();
-ex1.buildBoard(4);
+ex1.buildBoard(8);
+console.log(ex2.min(0, 10));
