09ab554e57a76d68f8ae11edc405d227cdd012f6
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-08-31 14:56:49 -0300

    Implementing a queue

---
+++ queue/queue.js
@@ -0,0 +1,39 @@
+class Queue
+{
+  constructor()
+  {
+    this.items = [];
+  }
+
+  enqueue(item)
+  {
+    this.items.push(item);
+  }
+
+  dequeue()
+  {
+    if(this.isEmpty())
+      return -1;
+    return this.items.shift();
+  }
+
+  front()
+  {
+    if(this.isEmpty())
+      return -1
+    return this.items[0];
+  }
+
+  isEmpty()
+  {
+    return this.items.length == 0;
+  }
+  
+  printQueue()
+  {
+    var str = "";
+    for(var i = 0; i < this.items.length; i++)
+      str += this.items[i] + " ";
+    return str;
+  }
+}
